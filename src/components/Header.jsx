8d285c1e11e5bb141c6a95665c4aaff2773a6841
Hachikoi-the-creator <facka72@gmail.com>
2022-02-23 19:13:12 -0600

    navbar updated :D

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,35 @@ export default function Header() {
   const [burger, setBurger] = React.useState(false);
 
   return (
-    <div>
+    <div className="header">
+      {/* BURGER */}
       <div
+        id="test"
         className={`menu-btn ${burger ? "open" : ""}`}
         onClick={() => setBurger(!burger)}
       >
         <div className="menu-btn__burger"></div>
       </div>
+
+      {/* NAVBAR */}
+      <ul className={`navbar ${burger ? "" : "hidde-left"}`}>
+        <li className="logo">
+          <a href="#">LOGO</a>
+        </li>
+        <li>
+          <a href="#">soma</a>
+        </li>
+        <li>
+          <a href="#">soma</a>
+        </li>
+        <li>
+          <a href="#">soma</a>
+        </li>
+        <li>
+          <a href="#">soma</a>
+        </li>
+      </ul>
+      <div className="test">7️⃣</div>
     </div>
   );
 }
