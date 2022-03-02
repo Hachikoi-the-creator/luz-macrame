4ec8ca262f838c75ea69eb115df9cb3916e21cf6
Hachikoi-the-creator <facka72@gmail.com>
2022-03-02 10:30:42 -0600

    kinda finished :D

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import "../sass/hero.scss";
 export default function Hero() {
   return (
     <div className="hero">
-      <h1>Manualidades Luz</h1>
+      <h1>Costura Luz</h1>
       <h2>Macrame, Uniformes, Manteles, etc.</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis unde
