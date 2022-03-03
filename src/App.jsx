fe17809ca80188baa209542a9ea6951d911e6d24
Hachikoi-the-creator <facka72@gmail.com>
2022-03-03 10:36:57 -0600

    fixed fatal typo :D

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sass/basics.scss";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Almoadas from "./components/Almoadas";
 import Macrame from "./components/Macrame";
